4481b7edf4054d36f9cb09960946386acb309593
yuliakot <93175658+yuliakot@users.noreply.github.com>
2023-07-07 00:31:13 -0500

    Update crt_lookup.rs

--- src/circuits/crt_lookup.rs
+++ src/circuits/crt_lookup.rs
@@ -1,6 +1,17 @@
 use super::* ;
+use std::collections::HashMap;
+use halo2_base::utils::{fe_to_biguint, biguint_to_fe};
 
-struct FLGateChip<F>(GateChip<F>);
+fn a_mod_p<F: ScalarField>(a: &F, p: &F) -> F{
+    biguint_to_fe::<F>(& (fe_to_biguint(a) % fe_to_biguint(p)))
+}
+
+fn into_crt<F: ScalarField>(a: F, moduli: Vec<F>) -> HashMap<&'static F, F>{
+    let res : HashMap<_, _> = moduli.iter().map(|x| (x, a_mod_p(&a, x))).collect();
+    res
+}
+
+struct FLGateChip<F: ScalarField>(GateChip<F>);
 
 impl<F: ScalarField> FLGateChip<F> {
     pub fn crt_lookup_mul(
@@ -9,10 +20,10 @@ impl<F: ScalarField> FLGateChip<F> {
         a: impl Into<QuantumCell<F>>,
         b: impl Into<QuantumCell<F>>,
         ab: impl Into<QuantumCell<F>>,
-         modulus: impl Into<QuantumCell<F>>,) -> AssignedValue<F>
+        modulus: impl Into<QuantumCell<F>>,) -> AssignedValue<F>
     {
-        let (a, b, ab) = into_crt(a, b, ab, modulus);
-        a
+        let [a, b, ab] = [a, b, ab].map(|x| a_mod_p(*x.into().value(), modulus.into().value()));
+        a.into()
     }
 
     pub fn crt_lookup_add(
@@ -23,7 +34,7 @@ impl<F: ScalarField> FLGateChip<F> {
         ab: impl Into<QuantumCell<F>>,
         modulus: impl Into<QuantumCell<F>>,) -> AssignedValue<F>
     {
-        let (a, b, ab) = into_crt(a, b, ab, modulus);
-        a
+        let [a, b, ab] = [a, b, ab].map(|x| a_mod_p(*x.into().value(), modulus.into().value()));
+        a.into()
     }
 }
