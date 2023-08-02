db6a79a6e5fadcd108ccb03811034c5c1feb71c7
yuliakotel <yuliakotel@gmail.com>
2023-08-02 03:06:13 -0500

    changed impl into quantumcell for 'assignedvalue'

--- src/multiplication_gates/crt_lookup.rs
+++ src/multiplication_gates/crt_lookup.rs
@@ -39,8 +39,8 @@ pub trait FLGateChip<F: ScalarField>{
     fn crt_lookup_division_with_remainder(
         &self,
         ctx: &mut Context<F>,
-        inputs : [impl Into<QuantumCell<F>> + Copy; 6],
-        p: impl Into<QuantumCell<F>> + Copy,
+        inputs : [AssignedValue<F>; 6],
+        p: AssignedValue<F>,
         modulus: F,) -> AssignedValue<F>;
 }
 
@@ -53,17 +53,17 @@ impl<F: ScalarField> FLGateChip<F> for GateChip<F>{
         a_times_b, 
         q, 
         p_times_q, 
-        r] : [impl Into<QuantumCell<F>> + Copy; 6],
-        p: impl Into<QuantumCell<F>> + Copy,
+        r] : [AssignedValue<F>; 6],
+        p: AssignedValue<F>,
         modulus: F,) -> AssignedValue<F>
     {
-        let res1 = self.crt_lookup_mul(ctx, a.into(), b.into(), a_times_b.into(), modulus);
-        let res2 = self.crt_lookup_mul(ctx, p.into(), q.into(), p_times_q.into(), modulus);
-        let res3 = self.crt_lookup_add(ctx, p_times_q.into().clone(), r.into(), a_times_b.into().clone(), modulus);
+        let res1 = self.crt_lookup_mul(ctx, a, b, a_times_b, modulus);
+        let res2 = self.crt_lookup_mul(ctx, p, q, p_times_q, modulus);
+        let res3 = self.crt_lookup_add(ctx, p_times_q, r, a_times_b, modulus);
         let res4 = self.and(ctx, res1, res2);
         let res5 = self.and(ctx, res4, res3);
 
-        println!("\nmodulus = {:?}, \na = {:?}, \nb = {:?}, \np = {:?}, \nq = {:?}, \nr = {:?}", fe_to_biguint(&modulus), fe_to_biguint(a.into().value()), fe_to_biguint(b.into().value()), fe_to_biguint(p.into().value()), fe_to_biguint(q.into().value()), fe_to_biguint(r.into().value()));
+        //println!("\nmodulus = {:?}, \na = {:?}, \nb = {:?}, \np = {:?}, \nq = {:?}, \nr = {:?}", fe_to_biguint(&modulus), fe_to_biguint(&a.value), fe_to_biguint(&b.value), fe_to_biguint(&p.value), fe_to_biguint(&q.value), fe_to_biguint(&r.value));
         
         res5
     }
